d3b6fc1cfcbc618c191601563079dd949169b1e1
rzmy <46349755+alkhrzmy@users.noreply.github.com>
2019-06-17 01:41:17 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ def handle(self):
         content_type, chat_type, chat_id= telepot.glance(self)
         command = self['text'].strip().lower()
         if content_type == 'text' and chat_type == 'private':
-                url = 'https://secureapp.simsimi.com/v1/simsimi/talkset?uid=271898762&av=6.8.7.1&lc=id&cc=ID&tz=Asia%2FJakarta&os=a&ak=alofKTlYS08reLe4gzCD2W7mijs%3D&m$
+                url = 'https://secureapp.simsimi.com/v1/simsimi/talkset?uid=271898762&av=6.8.7.1&lc=id&cc=ID&tz=Asia%2FJakarta&os=a&ak=alofKTlYS08reLe4gzCD2W7mijs%3D&message_sentence='+self['text']+'&normalProb=2&isFilter=1&talkCnt=1&talkCntTotal=1&reqFilter=0&session=udu6auK1LfyAQwLzupMPbdKVYDX9HkLnkYkU4jPWFmX6MysmRf6uFqg5RhqqVzVjKixAJaoNuxLi1YNFjLrs2Qu3&triggerKeywords=%5B%5D'
                 headers = {
                         'User-Agent': 'Dalvik/2.1.0 (Linux; U; Android 6.0.1; ASUS_A007 Build/MMB29P)',
                         'Host': 'secureapp.simsimi.com',
