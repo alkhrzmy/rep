9762f2d1d8f6fb9dd7a93a107e1e403ddb61b90a
rzmy <46349755+alkhrzmy@users.noreply.github.com>
2019-06-17 01:45:44 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,12 @@ my_id = '694351915'
 def handle(self):
         content_type, chat_type, chat_id= telepot.glance(self)
         command = self['text'].strip().lower()
-        if content_type == 'text' and chat_type == 'private':
+        if command == '/check' and content_type == 'text' and chat_type == 'private':
+                timing = time.time()
+                elapseTime = time.time() - timing
+                elapseStart = time.time() - programStart
+                bot.sendMessage(chat_id, 'Bot already running ' + format_timespan(elapseStart) + '\nBot send message speed %f'%elapseTime)
+        else:
                 url = 'https://secureapp.simsimi.com/v1/simsimi/talkset?uid=271898762&av=6.8.7.1&lc=id&cc=ID&tz=Asia%2FJakarta&os=a&ak=alofKTlYS08reLe4gzCD2W7mijs%3D&message_sentence='+self['text']+'&normalProb=2&isFilter=1&talkCnt=1&talkCntTotal=1&reqFilter=0&session=udu6auK1LfyAQwLzupMPbdKVYDX9HkLnkYkU4jPWFmX6MysmRf6uFqg5RhqqVzVjKixAJaoNuxLi1YNFjLrs2Qu3&triggerKeywords=%5B%5D'
                 headers = {
                         'User-Agent': 'Dalvik/2.1.0 (Linux; U; Android 6.0.1; ASUS_A007 Build/MMB29P)',
@@ -27,13 +32,6 @@ def handle(self):
                 r = requests.get(url, headers=headers)
                 thedata = json.loads(r.text)
                 bot.sendMessage(chat_id, thedata['simsimi_talk_set']['answers'][0]['sentence'])
-        elif command == '/check':
-                timing = time.time()
-                elapseTime = time.time() - timing
-                elapseStart = time.time() - programStart
-                bot.sendMessage(chat_id, 'Bot already running ' + format_timespan(elapseStart) + '\nBot send message speed %f'%elapseTime)
-        else:
-                pass
 MessageLoop(bot, handle).run_as_thread()
 print ('Running...')
 # Keep the program running.
